dd4bfd4a1776acf354e99705b09f4b08ab5984d2
Sebastián Andrés <69599597+SebasAndres@users.noreply.github.com>
2022-10-20 23:17:28 -0300

    Add files via upload

--- tp1.hs
+++ tp1.hs
@@ -1,65 +1,63 @@
--- EJERCICIO 1. sonCoprimos
--- Idea: Evaluar si el mcd (a, b) == 1 con el Algoritmo de Euclides
+-- Tp 1
 
+-- Ejercicio 1
+-- Idea: Dos numeros a, b son coprimos si su mcd es 1 -> (a:b) = 1
+sonCoprimos :: Integer -> Integer -> Bool
+sonCoprimos a b | mcd a b == 1 = True
+                | otherwise = False
+
+-- Utilizamos el Algoritmo de Euclides para calcular el mcd.
+-- (a : b) = (b : a mod b) = ... = (n : 0) = n || (n : 1) = 1
 mcd :: Integer -> Integer -> Integer 
 mcd a b | b == 0 = abs(a)
         | b == 1 = 1
         | otherwise = mcd (b) (mod a b)
 
-sonCoprimos :: Integer -> Integer -> Bool
-sonCoprimos a b | (mcd a b == 1) = True
-                | otherwise = False
-
--- EJE3RCICO 2: es2Pseudoprimo
--- Idea: Resolvemos el problema para a_pseudoprimo y lo llevamos al caso particular
+-- Ejercicio 2
+-- Idea: Resolvemos el problema para un A generico (a-pseudoprimos) y luego lo aplicamos para 2-pseudoprimos.
+a_pseudoprimo :: Integer -> Integer -> Bool
+a_pseudoprimo a n | (esPrimo (n) == False) && (mod (a^(n-1)-1) n == 0) && (n /= 1) = True
+                  | otherwise = False 
 
 es2Pseudoprimo :: Integer -> Bool
-es2Pseudoprimo n = a_pseudoprimo n 2
-
-a_pseudoprimo :: Integer -> Integer -> Bool 
-a_pseudoprimo n a | (esPrimo(n) == False) && (mod (a^(n-1)-1) n == 0) = True
-                  | otherwise = False
-
--- EJERCICIO 3: cantidad3Pseudoprimos
--- Idea: recorrer todos los numeros de i hasta M y sumar uno cuando hay un pseudoprimo, terminar cuando i == M
--- Alternativa: recorrer los a_pseudoprimos[i] y parar cuando a[i] >= M. devolver i
-
-cantidad3Pseudoprimos :: Integer -> Integer
-cantidad3Pseudoprimos m = -1 + cantidad3PseudoprimosDesde 1 m 0
-
-cantidad3PseudoprimosDesde :: Integer -> Integer -> Integer -> Integer
-cantidad3PseudoprimosDesde i m c | i == m = c 
-                                 | es3Pseudoprimo (i) = cantidad3PseudoprimosDesde (i+1) m (c+1)   
-                                 | otherwise = cantidad3PseudoprimosDesde (i+1) m (c) 
+es2Pseudoprimo n = a_pseudoprimo 2 n
 
 es3Pseudoprimo :: Integer -> Bool
-es3Pseudoprimo n = a_pseudoprimo n 3
+es3Pseudoprimo n = a_pseudoprimo 3 n
 
--- EJERCICIO 4: kesimo2y3Pseudoprimo
--- Idea: Iterar hasta reducir k=0, incrementando el punto de partida d en cada vuelta d++
+-- Ejercicio 3
+-- Idea: Iteramos desde i=1 hasta m+1, con un contador que incrementa cuando i es 3Pseudoprimo.
+-- Cuando i > m, devolvemos el contador (valido para i-1, o sea i=m).  
+cantidad3Pseudoprimos :: Integer -> Integer
+cantidad3Pseudoprimos m = cantidad3PseudoprimosDesde 1 m 0
 
-kesimo2y3Pseudoprimo :: Integer -> Integer -- devuelve el k-esimo numero que es 2-pseudoprimo y 3-pseudoprimo
+cantidad3PseudoprimosDesde :: Integer -> Integer -> Integer -> Integer
+cantidad3PseudoprimosDesde i m c | i > m = c 
+                                 | es3Pseudoprimo (i) = cantidad3PseudoprimosDesde (i+1) m (c+1) 
+                                 | otherwise = cantidad3PseudoprimosDesde (i+1) m c
+
+-- Ejercicio 4
+-- Idea: Recorremos desde i=2 hasta que el contador c llegue a cero (c=0 -> fin),
+-- Le restamos uno a c cuando i cumple propiedad de ser 2 y 3 pseudoprimo.
+kesimo2y3Pseudoprimo :: Integer -> Integer
 kesimo2y3Pseudoprimo n = kesimo2y3PseudoprimoDesde 2 n
 
-kesimo2y3PseudoprimoDesde :: Integer -> Integer -> Integer 
-kesimo2y3PseudoprimoDesde d k | (k == 0) = d-1 -- porque k llego a cero en el paso anterior
-                              | (es2Pseudoprimo d && es3Pseudoprimo d) = kesimo2y3PseudoprimoDesde (d+1) (k-1)
-                              | otherwise = kesimo2y3PseudoprimoDesde (d+1) (k)
+kesimo2y3PseudoprimoDesde :: Integer -> Integer -> Integer
+kesimo2y3PseudoprimoDesde i c | (c == 0) = i-1
+                              | (es2Pseudoprimo (i) && es3Pseudoprimo(i)) = kesimo2y3PseudoprimoDesde (i+1) (c-1)
+                              | otherwise = kesimo2y3PseudoprimoDesde (i+1) (c)
 
--- EJERCICIO 5: esCarmichael
--- Idea: Recorro los numeros desde i=1 a n, y evaluo que la condicion se cumpla en los i coprimos con n.
--- Si llega a i=n-1 y nunca incumplio la condicion (i,n coprimos -> n es i-pseudoprimo), devuelve True 
-
+-- Ejercicio 5
+-- Idea: Recorrer desde d=1 hasta d=n-1 y verificar que cuando d sea coprimo con n, n sea d-pseudoprimo :: Definicion de Carmichael.
 esCarmichael :: Integer -> Bool
-esCarmichael n = esCarmichaelDesde 1 n
+esCarmichael n = esCarmichaelDesde 1 (n)
 
 esCarmichaelDesde :: Integer -> Integer -> Bool
-esCarmichaelDesde d n | (sonCoprimos d n == True) && (a_pseudoprimo n d == False) = False -- coprimo pero no apseudoprimo  
-                      | (d == n-1) = (sonCoprimos d n == True) && (a_pseudoprimo n d == True) 
+esCarmichaelDesde d n | (sonCoprimos d n) && (a_pseudoprimo d n == False) = False
+                      | (d == n-1) = (sonCoprimos d n) && (a_pseudoprimo d n) || (sonCoprimos d n == False)
                       | otherwise = esCarmichaelDesde (d+1) n
 
--------------------------------------------------------
---- Funciones Auxiliares (Implementadas en Clase) -----
+-- Funciones auxiliares (Implementadas en clase)
 
 menorDivisor :: Integer -> Integer
 menorDivisor n = menorDivisorDesde n 2
